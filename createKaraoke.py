1aeb151d55387d33d6cf1858add015b7bdd1ffa8
Alfie Powers <powersalfie@gmail.com>
2020-06-21 18:38:22 +0100

    Generating songs

--- createKaraoke.py
+++ createKaraoke.py
@@ -3,8 +3,13 @@
 import subprocess
 import sys, os, getopt, csv, math, re
 
-song_file = ""
-subtitles = ""
+details = {
+    "song_file": "",
+    "subtitles": "",
+    "song_name": "",
+}
+
+
 version = 1.0
 bpm = 100
 lowest_freq = 32.7032 # This is Peddle C (C1)
@@ -17,7 +22,7 @@ def createUltrastarMap(pitch, song_length):
     print("Generating UltraStar map file - We need some extra info!")
     ema_pitch = []
     ema_pitch.append(float(pitch[0]))
-    alpha = 0.2
+    alpha = 0.4
     index = 1
     for p in pitch:
         if len(ema_pitch) < len(pitch):
@@ -39,10 +44,34 @@ def createUltrastarMap(pitch, song_length):
         frequencies.append(current_freq)
         n += 1
 
-
+    ultraStarFreqs = list()
     for i in range(0,totalBeats,1):
-        index = getPitchIndex(i, bpm)
+        freqValue = float(pitch[getPitchIndex(i, bpm)])
+        j = 0
+        while(frequencies[j+1] < freqValue):
+            j += 1
+        ultraStarFreqs.append(j)
 
+    file = open("song.txt", "a")
+    # Add the file metadata
+    meta_data = ""
+
+
+
+
+    # We now parse the beats into a valid Karaoke map
+    i = 0
+    while i < totalBeats:
+        line = ": " + str(i)
+        j = 1
+        if i + j < len(ultraStarFreqs):
+            while(ultraStarFreqs[i] == ultraStarFreqs[i+j]):
+                j += 1
+                if i + j >= len(ultraStarFreqs):
+                    break
+        line = line + " " + str(j) + " " + str(ultraStarFreqs[i]) + " ba" + "\n"
+        i += j
+        file.write(line)
 
 
 
@@ -61,13 +90,13 @@ def main(argv):
             print("createKaraoke.py -i <inputSongFile> -s <subtitleFile>")
             sys.exit()
         elif opt in ('-i', "--ifile"):
-            song_file = arg
+            details["song_file"] = arg
         elif opt in ('-s', "--sfile"):
-            subtitles = arg
+            details["subtitles"] = arg
     # Load in the songfile and subtitles to check they were valid
 
     # Get the total song length
-    args = ("ffprobe", "-show_entries", "format=duration", "-i", song_file)
+    args = ("ffprobe", "-show_entries", "format=duration", "-i", details["song_file"])
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
     song_length = re.findall(r'\d*\.?\d*', popen.stdout.read().decode("utf-8"))
@@ -87,17 +116,17 @@ def main(argv):
         print("AutoKaraoke needs to download Spleeter models. This may take a few minutes.")
 
     print("Isolating vocals from instrumentals.")
-    spleeter_cmd = "spleeter separate -i" + song_file + " -p spleeter:2stems -o output"
+    spleeter_cmd = "spleeter separate -i" + details["song_file"] + " -p spleeter:2stems -o output"
     #os.system(spleeter_cmd)
 
     print("Estimating pitch values. This will take some time based on the power of your CPU!\n"
           "Using GPU tensorflow will lead to better performance: https://www.tensorflow.org/install/gpu ")
-    s = os.path.splitext(song_file)
-    crepe_cmd = "crepe output/" + os.path.splitext(song_file)[0] + "/vocals.wav"
-    # os.system(crepe_cmd)
+    s = os.path.splitext(details["song_file"])
+    crepe_cmd = "crepe output/" + os.path.splitext(details["song_file"])[0] + "/vocals.wav"
+    #os.system(crepe_cmd)
 
     # All heavy data processing has been completed, let us load in the results
-    csv_loc = "output/" + os.path.splitext(song_file)[0] + "/vocals.f0.csv"
+    csv_loc = "output/" + os.path.splitext(details["song_file"])[0] + "/vocals.f0.csv"
 
     # Output from crepe has 3 columns:
     # Time (10ms intervals), Predicted pitch, Confidence interval
